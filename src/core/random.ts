19cf0613ee988c83d9ac5c2486b5bbb9b9b3b69a
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-06-13 15:24:36 +0200

    Super Farmer: basic utils for dice rolling

---
+++ src/core/random.ts
@@ -0,0 +1,7 @@
+
+/**
+ * Returns a random integer from zero to bound - 1.
+ */
+export function randomInteger(bound: number) {
+    return Math.floor(Math.random() * bound)
+}
---
+++ src/games/superFarmer/README.md
@@ -0,0 +1,4 @@
+# Super Farmer game simulation tools
+
+## About the game
+[Board Game Geek description](https://boardgamegeek.com/boardgame/17557/super-farmer)
